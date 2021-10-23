6759497b7931042564121f9a943c842ad7d41df5
sullya974 <3134418+sullya974@users.noreply.github.com>
2021-10-23 21:39:54 +0200

    First commit

---
+++ app/src/main/java/com/sully/tarotapp/View/RestPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.sully.tarotapp.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sully.tarotapp.R
+
+class RestPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rest_password)
+    }
+}
